d0248263374b965221efaeb9cdd33cd5573ac459
urb <urb@skipnix.zurich.ibm.com>
2020-01-09 14:00:10 +0100

    add remove cdc schema tables are case sensitive

--- debezium-connector-db2/src/test/docker/db2-cdc-docker/asncdcaddremove.sql
+++ debezium-connector-db2/src/test/docker/db2-cdc-docker/asncdcaddremove.sql
@@ -1,4 +1,10 @@
 
+-- 
+-- Define ASNCDC.REMOVETABLE() and ASNCDC.ADDTABLE()
+-- ASNCDC.ADDTABLE() puts a table in CDC mode, making the ASNCapture server collect changes for the table
+-- ASNCDC.REMOVETABLE() makes the ASNCapture server stop collecting changes for that table
+--
+
 --#SET TERMINATOR @
 CREATE OR REPLACE PROCEDURE ASNCDC.REMOVETABLE(
 in  tableschema VARCHAR(128),
@@ -15,16 +21,7 @@ DECLARE SQLSTATE CHAR(5);
 DECLARE RC_SQLCODE INT DEFAULT 0; 
 DECLARE RC_SQLSTATE CHAR(5) DEFAULT '00000';
 
-
-
 DECLARE CONTINUE HANDLER FOR SQLEXCEPTION, SQLWARNING, NOT FOUND  VALUES (SQLCODE, SQLSTATE) INTO RC_SQLCODE, RC_SQLSTATE; 
-
-
-
-
-
-
-
 
 -- delete ASN.IBMSNAP_PRUNCTL entries / source
 SET stmtSQL = 'DELETE FROM ASNCDC.IBMSNAP_PRUNCNTL WHERE SOURCE_OWNER=''' || tableschema || ''' AND SOURCE_TABLE=''' || tablename ||  '''';
@@ -35,8 +32,8 @@ SET stmtSQL = 'DELETE FROM ASNCDC.IBMSNAP_REGISTER WHERE SOURCE_OWNER=''' || tab
         EXECUTE IMMEDIATE stmtSQL;
 
 -- drop CD Table / source
-SET stmtSQL = 'DROP TABLE ASNCDC.CDC_' ||
-                tableschema || '_' || tablename ;
+SET stmtSQL = 'DROP TABLE ASNCDC."CDC_' ||
+                tableschema || '_' || tablename || '"';
         EXECUTE IMMEDIATE stmtSQL;
 
 --  delete ASN.IBMSNAP_SUBS_COLS entries /target
@@ -50,21 +47,16 @@ SET stmtSQL = 'DELETE FROM ASNCDC.IBMSNAP_SUBS_MEMBR WHERE TARGET_OWNER=''' || t
 -- delete ASN.IBMQREP_COLVERSION
 SET stmtSQL = 'DELETE FROM ASNCDC.IBMQREP_COLVERSION col WHERE EXISTS (SELECT * FROM ASNCDC.IBMQREP_TABVERSION tab WHERE SOURCE_OWNER=''' || tableschema || ''' AND SOURCE_NAME=''' || tablename ||  '''AND col.TABLEID1 = tab.TABLEID1 AND col.TABLEID2 = tab.TABLEID2';
         EXECUTE IMMEDIATE stmtSQL;
-        
+
 -- delete ASN.IBMQREP_TABVERSION
 SET stmtSQL = 'DELETE FROM ASNCDC.IBMQREP_TABVERSION WHERE SOURCE_OWNER=''' || tableschema || ''' AND SOURCE_NAME=''' || tablename ||  '''';
         EXECUTE IMMEDIATE stmtSQL;
 
-
-
-SET  stmtSQL = 'ALTER TABLE ' || tableschema || '.' || tablename ||  ' DATA CAPTURE NONE';
+SET  stmtSQL = 'ALTER TABLE "' || tableschema || '"."' || tablename ||  '" DATA CAPTURE NONE';
 EXECUTE IMMEDIATE stmtSQL; 
-
 
 END P1@
 --#SET TERMINATOR ;
-
-
 
 --#SET TERMINATOR @
 CREATE OR REPLACE PROCEDURE ASNCDC.ADDTABLE(
@@ -79,48 +71,44 @@ DECLARE SQLSTATE CHAR(5);
 
 DECLARE stmtSQL VARCHAR(2048);
 
-
-SET  stmtSQL = 'ALTER TABLE ' || tableschema || '.' || tablename ||  ' DATA CAPTURE CHANGES';
+SET  stmtSQL = 'ALTER TABLE "' || tableschema || '"."' || tablename || '" DATA CAPTURE CHANGES';
 EXECUTE IMMEDIATE stmtSQL; 
 
-
-SET  stmtSQL = 'CREATE TABLE ASNCDC.CDC_' ||
-                tableschema || '_' || tablename || 
+SET  stmtSQL = 'CREATE TABLE ASNCDC."CDC_' ||
+                tableschema || '_' || tablename || '"' || 
                 ' AS ( SELECT ' || 
                 '   CAST('''' AS VARCHAR ( 16 )  FOR BIT DATA) AS  IBMSNAP_COMMITSEQ, ' || 
                 '   CAST('''' AS VARCHAR ( 16 )  FOR BIT DATA) AS  IBMSNAP_INTENTSEQ, ' ||
                 '   CAST ('''' AS CHAR(1)) ' || 
-                ' AS IBMSNAP_OPERATION,   t.* FROM ' || tableschema || '.' || tablename || ' as t )  WITH NO  DATA ORGANIZE BY ROW ';
+                ' AS IBMSNAP_OPERATION,   t.* FROM "' || tableschema || '"."' || tablename || '" as t )  WITH NO  DATA ORGANIZE BY ROW ';
 EXECUTE IMMEDIATE stmtSQL; 
 
-
-SET stmtSQL = 'ALTER TABLE ASNCDC.CDC_' ||
-                tableschema || '_' || tablename || 
+SET stmtSQL = 'ALTER TABLE ASNCDC."CDC_' ||
+                tableschema || '_' || tablename || '"' ||
                 ' ALTER COLUMN IBMSNAP_COMMITSEQ SET NOT NULL';
 EXECUTE IMMEDIATE stmtSQL; 
 
-SET stmtSQL = 'ALTER TABLE ASNCDC.CDC_' ||
-                tableschema || '_' || tablename || 
+SET stmtSQL = 'ALTER TABLE ASNCDC."CDC_' ||
+                tableschema || '_' || tablename || '"' || 
               ' ALTER COLUMN IBMSNAP_INTENTSEQ SET NOT NULL';
 EXECUTE IMMEDIATE stmtSQL; 
 
-SET stmtSQL = 'ALTER TABLE ASNCDC.CDC_' ||
-                tableschema || '_' || tablename || 
+SET stmtSQL = 'ALTER TABLE ASNCDC."CDC_' ||
+                tableschema || '_' || tablename || '"' ||
               ' ALTER COLUMN IBMSNAP_OPERATION SET NOT NULL';
 EXECUTE IMMEDIATE stmtSQL;
 
-SET stmtSQL = 'CREATE  UNIQUE  INDEX ASNCDC.IXCDC_' ||
-                tableschema || '_' || tablename || 
-                ' ON ASNCDC.CDC_' ||
-                tableschema || '_' || tablename || 
+SET stmtSQL = 'CREATE  UNIQUE  INDEX ASNCDC."IXCDC_' ||
+                  tableschema || '_' || tablename || '"' ||
+                ' ON ASNCDC."CDC_' ||
+                  tableschema || '_' || tablename || '"' ||
                 ' ( IBMSNAP_COMMITSEQ ASC, IBMSNAP_INTENTSEQ ASC ) PCTFREE 0 MINPCTUSED 0';
 EXECUTE IMMEDIATE stmtSQL;
 
-SET stmtSQL = 'ALTER TABLE ASNCDC.CDC_' ||
-                tableschema || '_' || tablename || 
+SET stmtSQL = 'ALTER TABLE ASNCDC."CDC_' ||
+                tableschema || '_' || tablename || '"' || 
               '  VOLATILE CARDINALITY';
 EXECUTE IMMEDIATE stmtSQL;
-
 
 SET stmtSQL =   'INSERT INTO ASNCDC.IBMSNAP_REGISTER (SOURCE_OWNER, SOURCE_TABLE, ' || 
                 'SOURCE_VIEW_QUAL, GLOBAL_RECORD, SOURCE_STRUCTURE, SOURCE_CONDENSED, ' || 
@@ -165,7 +153,6 @@ SET stmtSQL =   'INSERT INTO ASNCDC.IBMSNAP_REGISTER (SOURCE_OWNER, SOURCE_TABLE
                 'null ) ';
 EXECUTE IMMEDIATE stmtSQL;
 
-
 SET stmtSQL =   'INSERT INTO ASNCDC.IBMSNAP_PRUNCNTL ( ' || 
                 'TARGET_SERVER,  ' || 
                 'TARGET_OWNER,  ' || 
@@ -198,11 +185,10 @@ SET stmtSQL =   'INSERT INTO ASNCDC.IBMSNAP_PRUNCNTL ( ' ||
                 '8, ' || 
                 ' (Select CURRENT_SERVER from sysibm.sysdummy1 ), ' || 
                 '''ASNCDC'', ' || 
-                '''CDC_' ||  tableschema ||  '_' || tablename || ''', ' ||
+                '''"CDC_' ||  tableschema ||  '_' || tablename || '"'', ' ||
                 ' ( SELECT CASE WHEN max(CAST(MAP_ID AS INT)) IS NULL THEN CAST(1 AS VARCHAR(10)) ELSE CAST(CAST(max(MAP_ID) AS INT) + 1 AS VARCHAR(10))  END AS MYINT from  ASNCDC.IBMSNAP_PRUNCNTL ) ' || 
                 '    )';
 EXECUTE IMMEDIATE stmtSQL;
 
 END P1@
 --#SET TERMINATOR ;
-
