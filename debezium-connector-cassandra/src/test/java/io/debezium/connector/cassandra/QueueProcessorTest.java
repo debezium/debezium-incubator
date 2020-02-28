14f8a40a03a9900abb6760b3936dd3243f21cf44
Bingqin Zhou <zhou.bing@husky.neu.edu>
2020-02-28 11:29:25 -0800

    DBZ_1820 Try to fix build issue.

--- debezium-connector-cassandra/src/test/java/io/debezium/connector/cassandra/QueueProcessorTest.java
+++ debezium-connector-cassandra/src/test/java/io/debezium/connector/cassandra/QueueProcessorTest.java
@@ -6,7 +6,6 @@
 package io.debezium.connector.cassandra;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
