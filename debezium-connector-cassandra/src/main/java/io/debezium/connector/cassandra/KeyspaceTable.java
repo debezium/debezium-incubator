acd41aa2d44731d2ac9f242e9c101275aabebc82
Bingqin Zhou <zhou.bing@husky.neu.edu>
2020-03-31 17:14:47 -0700

    DBZ-1407 Replace custom CassandraTopicSelector with Debezium TopicSelector.

--- debezium-connector-cassandra/src/main/java/io/debezium/connector/cassandra/KeyspaceTable.java
+++ debezium-connector-cassandra/src/main/java/io/debezium/connector/cassandra/KeyspaceTable.java
@@ -5,6 +5,8 @@
  */
 package io.debezium.connector.cassandra;
 
+import io.debezium.schema.DataCollectionId;
+
 import java.util.Objects;
 
 import com.datastax.driver.core.TableMetadata;
@@ -12,7 +14,7 @@ import com.datastax.driver.core.TableMetadata;
 /**
  * The KeyspaceTable uniquely identifies each table in the Cassandra cluster
  */
-public class KeyspaceTable {
+public class KeyspaceTable implements DataCollectionId {
     public final String keyspace;
     public final String table;
 
@@ -50,5 +52,10 @@ public class KeyspaceTable {
     @Override
     public String toString() {
         return name();
+    }
+
+    @Override
+    public String identifier() {
+        return keyspace + "." + table;
     }
 }
