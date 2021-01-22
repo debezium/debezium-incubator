dcb43d69a18b345dc372e05a2378f350502d2236
Chris Cranford <chris@hibernate.org>
2021-01-22 18:05:15 -0500

    DBZ-2711 Implement table-level ALL Supplemental Data Logging Validation

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/LogMinerStreamingChangeEventSource.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/logminer/LogMinerStreamingChangeEventSource.java
@@ -140,7 +140,7 @@ public class LogMinerStreamingChangeEventSource implements StreamingChangeEventS
                 }
 
                 setNlsSessionParameters(jdbcConnection);
-                checkSupplementalLogging(jdbcConnection, connectorConfig.getPdbName());
+                checkSupplementalLogging(jdbcConnection, connectorConfig.getPdbName(), schema);
 
                 initializeRedoLogsForMining(connection, false, archiveLogRetention);
 
